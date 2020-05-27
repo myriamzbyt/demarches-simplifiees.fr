410b82539df2fff40bf5e55063e2eef5945f0f82
clemkeirua <clement@keiruaprod.fr>
2020-05-27 11:40:57 +0200

    revert changes to downloadable file

--- app/lib/active_storage/downloadable_file.rb
+++ app/lib/active_storage/downloadable_file.rb
@@ -1,6 +1,4 @@
 class ActiveStorage::DownloadableFile
-  attr_reader :url
-
   def initialize(attached)
     if using_local_backend?
       @url = 'file://' + ActiveStorage::Blob.service.path_for(attached.key)
@@ -13,18 +11,7 @@ class ActiveStorage::DownloadableFile
     pjs = PiecesJustificativesService.liste_pieces_justificatives(dossier)
     pjs.map do |piece_justificative|
       [
-        ActiveStorage::DownloadableFile.new(piece_justificative),
-        "dossier-#{dossier.id}/#{piece_justificative.filename}"
-      ]
-    end
-  end
-
-  def self.create_attachment_list_from_dossier(dossier)
-    pjs = PiecesJustificativesService.liste_pieces_justificatives(dossier)
-    pjs.map do |piece_justificative|
-      [
-        piece_justificative,
-        "dossier-#{dossier.id}/#{piece_justificative.filename}"
+        ActiveStorage::DownloadableFile.new(piece_justificative)
       ]
     end
   end
