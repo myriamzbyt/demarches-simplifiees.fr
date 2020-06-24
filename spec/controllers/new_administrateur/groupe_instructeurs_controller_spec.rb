d0b108b19d9b5c15f8349f07f2da49abf6cba151
kara Diaby <kdiaby.pro@gmail.com>
2020-06-24 19:04:39 +0200

    fixup! fixup! fixup! fixup! fixup! Move admin/assigns to the new UI like groupe_instructeurs

--- spec/controllers/new_administrateur/groupe_instructeurs_controller_spec.rb
+++ spec/controllers/new_administrateur/groupe_instructeurs_controller_spec.rb
@@ -31,6 +31,22 @@ describe NewAdministrateur::GroupeInstructeursController, type: :controller do
 
       it { expect(response).to have_http_status(:ok) }
     end
+
+    context 'when the routage is not activated on the procedure' do
+      let(:procedure) { create :procedure, administrateur: admin, instructeurs: [instructeur_assigned_1, instructeur_assigned_2] }
+      let!(:instructeur_assigned_1) { create :instructeur, email: 'instructeur_1@ministere_a.gouv.fr', administrateurs: [admin] }
+      let!(:instructeur_assigned_2) { create :instructeur, email: 'instructeur_2@ministere_b.gouv.fr', administrateurs: [admin] }
+      let!(:instructeur_not_assigned_1) { create :instructeur, email: 'instructeur_3@ministere_a.gouv.fr', administrateurs: [admin] }
+      let!(:instructeur_not_assigned_2) { create :instructeur, email: 'instructeur_4@ministere_b.gouv.fr', administrateurs: [admin] }
+      subject! { get :show, params: { procedure_id: procedure.id, id: gi_1_1.id } }
+
+      it { expect(response.status).to eq(200) }
+
+      it 'sets the assigned and not assigned instructeurs' do
+        expect(assigns(:instructeurs)).to match_array([instructeur_assigned_1, instructeur_assigned_2])
+        expect(assigns(:available_instructeur_emails)).to match_array(['instructeur_3@ministere_a.gouv.fr', 'instructeur_4@ministere_b.gouv.fr'])
+      end
+    end
   end
 
   describe '#create' do
@@ -186,6 +202,34 @@ describe NewAdministrateur::GroupeInstructeursController, type: :controller do
 
       it { expect(gi_1_1.reload.label).not_to eq(new_name) }
       it { expect(flash.alert).to be_present }
+    end
+  end
+
+  describe '#add_instructeur_no_routage' do
+    let(:procedure) { create :procedure, administrateur: admin }
+    let(:emails) { ['instructeur_3@ministere_a.gouv.fr', 'instructeur_4@ministere_b.gouv.fr'] }
+    subject { post :add_instructeur_no_routage, params: { emails: emails, procedure_id: procedure.id, id: gi_1_1.id } }
+
+    context 'when all emails are valid' do
+      let(:emails) { ['test@b.gouv.fr', 'test2@b.gouv.fr'] }
+      it { expect(response.status).to eq(200) }
+      it { expect(subject.request.flash[:alert]).to be_nil }
+      it { expect(subject.request.flash[:notice]).to be_present }
+      it { expect(subject).to redirect_to procedure_groupe_instructeur_path(procedure, procedure.defaut_groupe_instructeur) }
+    end
+
+    context 'when there is at least one bad email' do
+      let(:emails) { ['badmail', 'instructeur2@gmail.com'] }
+      it { expect(response.status).to eq(200) }
+      it { expect(subject.request.flash[:alert]).to be_present }
+      it { expect(subject.request.flash[:notice]).to be_present }
+      it { expect(subject).to redirect_to procedure_groupe_instructeur_path(procedure, procedure.defaut_groupe_instructeur) }
+    end
+
+    context 'when the admin wants to assign an instructor who is already assigned on this procedure' do
+      let(:emails) { ['instructeur_1@ministere_a.gouv.fr'] }
+      it { expect(subject.request.flash[:alert]).to be_present }
+      it { expect(subject).to redirect_to procedure_groupe_instructeur_path(procedure, procedure.defaut_groupe_instructeur) }
     end
   end
 
